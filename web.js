c0f682ded69326c06e4241d2ffba2476f3338b04
Arun Bhusal <bhusalarun7@gmail.com>
2022-11-23 20:30:39 +0545

    initial Commit

---
+++ web.js
@@ -0,0 +1,61 @@
+solc = require("solc");
+
+fs = require("fs");
+
+Web3 = require("web3");
+
+let web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+
+let fileContent = fs.readFileSync("demo.sol").toString();
+
+
+console.log(fileContent);
+
+var input = {
+    language: "Solidity",
+    sources: {
+        "demo.sol": {
+            content: fileContent,
+        },
+    },
+
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["*"],
+            },
+        },
+    },
+};
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+console.log("Output", output);
+
+ABI = output.contracts["demo.sol"]["demo"].abi;
+bytecode = output.contracts["demo.sol"]["demo"].evm.bytecode.object;
+console.log("abi:", ABI);
+console.log("bytecode", bytecode);
+
+
+
+contract = new web3.eth.Contract(ABI);
+
+web3.eth.getAccounts().then((accounts) => {
+    console.log("Accounts:", accounts);
+    defaultAccount = accounts[0];
+    console.log("Default Account:", defaultAccount);
+
+    contract
+        .deploy({ data: bytecode })
+        .send({ from: defaultAccount, gas: 500000 })
+        .on("receipt", (receipt) => {
+            console.log("Contract Address:", receipt.contractAddress);
+        })
+        .then((demoContract) => {
+            demoContract.methods.x().call((err, data) => {
+                console.log("Initial Value:", data);
+            });
+        });
+
+});
